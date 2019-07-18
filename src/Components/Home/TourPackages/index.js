094b1252dada3f2081dd2f41f94915f6807c706b
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-18 13:51:18 +0800

    Decrease height of tourpackage card

--- src/Components/Home/TourPackages/index.js
+++ src/Components/Home/TourPackages/index.js
@@ -6,7 +6,7 @@ import HeaderText from '../HeaderText';
 
 const TourPackagesWrapper = styled.div`
   display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
   grid-gap: 10px;
 `
 
