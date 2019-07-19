8c5d176109f1f3f9d98e1b413b04cf2059015e9e
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-19 13:09:55 +0800

    Remove banner photo in Tourpackage page

--- src/Components/TourPackage/modules/TourPackageInfo/TourPackageInfo.js
+++ src/Components/TourPackage/modules/TourPackageInfo/TourPackageInfo.js
@@ -1,18 +1,10 @@
 import React from 'react';
 
-import {Image} from 'cloudinary-react';
-
 import './TourPackageInfo.css';
 
 const TourPackageInfo = props => {
     return (
      <div className="tourpackage_info_wrapper">
-      <Image 
-        cloudName="etours-davao-inc"
-        id="tourpackage_banner"
-        width="650"
-        crop="thumb"
-        publicId={props.data.photo} />
        <h1 id="tourpackage_title">{props.data.name}</h1>
        <p id="tourpackage_code_duration">{props.data.duration_text}</p>
        <p id="tourpackage_description">{props.data.description}</p>
