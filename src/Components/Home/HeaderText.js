fbec0a610ea050170ffe04d57aa9e566b023e26a
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-30 07:26:10 +0800

    Decrease font-size

--- src/Components/Home/HeaderText.js
+++ src/Components/Home/HeaderText.js
@@ -2,8 +2,7 @@ import styled from 'styled-components';
 
 export default styled.h2`
   font-family: 'Gotham Bold',Impact, 'Haettenschweiler', 'Arial Narrow Bold', sans-serif;
-  font-size: 2.5rem;
-  padding-top: 1rem;
-  padding-bottom: 1rem;
+  font-size: 1.5rem;
+  padding: .5rem;
   color: #e67e22;
 `
