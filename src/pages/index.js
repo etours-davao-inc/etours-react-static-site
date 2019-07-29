f6832556b774581316d5af615e4306e21d29dd02
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-29 15:49:34 +0800

    Add kadayawan video banner, re arrange sections

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,14 +19,20 @@ export default withRouteData(props => {
         <meta name="description" content={data.Header.metaDescription} />
       </Head>
       <span>
-        <Banner text={data.Header.bannerText} />
+        <video controls="controls" loop="loop" poster="" style={{width: '100%', height: 'auto'}}>
+          <source src="https://res.cloudinary.com/etours-davao-inc/video/upload/v1564382116/Etours_Kadayawan_Cover_Video_2019.webm" type="video/webm" />
+          <source src="https://res.cloudinary.com/etours-davao-inc/video/upload/v1564382116/Etours_Kadayawan_Cover_Video_2019.mp4" type="video/mp4" />
+          <source src="https://res.cloudinary.com/etours-davao-inc/video/upload/v1564382116/Etours_Kadayawan_Cover_Video_2019.ogv" type="video/ogg" />
+          Your browser does not support playing video content.
+        </video>
         <ResponsiveWidth>
           <TourPackages data={props.tourpackages} settings={data} />
+          <Banner text={data.Header.bannerText} />
           <Services data={data} />
           <Values data={data.Values} />
           <About />
         </ResponsiveWidth>
       </span>
-    </React.Fragment>
+    </React.Fragment >
   )
 })
