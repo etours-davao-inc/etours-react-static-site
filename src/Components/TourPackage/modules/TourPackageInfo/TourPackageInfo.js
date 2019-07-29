abdcde02870e5e3027e2602d543691c4b5c92bae
Tom Claudio <t0mclaudio@Toms-MacBook-Pro.local>
2019-07-29 16:01:24 +0800

    Include terms and responsibilities as tourpackage info

--- src/Components/TourPackage/modules/TourPackageInfo/TourPackageInfo.js
+++ src/Components/TourPackage/modules/TourPackageInfo/TourPackageInfo.js
@@ -5,12 +5,15 @@ import './TourPackageInfo.css';
 const TourPackageInfo = props => {
     return (
      <div className="tourpackage_info_wrapper">
-       <h1 id="tourpackage_title">{props.data.name}</h1>
-       <span>Last Updated: {props.lastUpdate}</span>
-       <p id="tourpackage_code_duration">{props.data.duration_text}</p>
+       <h1 id="tourpackage_title" className="m-0 pb-0">{props.data.name}</h1>
+       <span><small>Last Updated: {props.lastUpdate}</small></span>
+       <p id="tourpackage_code_duration" className="mt-3">{props.data.duration_text}</p>
        <p id="tourpackage_description">{props.data.description}</p>
-       <div id="tourpackage_info_wrapper" dangerouslySetInnerHTML={{ __html: props.data.info }}>
-       </div>
+       <div id="tourpackage_info_wrapper" dangerouslySetInnerHTML={{ __html: props.data.info }} />
+       <h3 style={{ color: '#d35400', fontSize: '1em'}}>Terms and conditions</h3>
+       <div id="tourpackage_info_wrapper" dangerouslySetInnerHTML={{ __html: props.data.terms }} />
+       <h3 style={{ color: '#d35400', fontSize: '1em'}}>Responsibilities</h3>
+       <div id="tourpackage_info_wrapper" dangerouslySetInnerHTML={{ __html: props.data.responsibilites }} />
      </div>
     )
 }
