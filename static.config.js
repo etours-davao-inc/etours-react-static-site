5f28e22acdc2ae283f6f322cc5cd34148deea2cc
Tom Claudio <dev.tomclaudio@gmail.com>
2023-04-01 11:06:46 +0800

    update site root

--- static.config.js
+++ static.config.js
@@ -1,49 +1,48 @@
-import React from 'react';
-import path from 'path'
-import { dataSource, urlPrefix } from './data';
+import React from "react";
+import path from "path";
+import { dataSource, urlPrefix } from "./data";
 
-import { format } from 'date-fns';
+import { format } from "date-fns";
 export default {
-  siteRoot: 'https://www.etours.ph',
+  siteRoot: "./",
   minLoadTime: 2500,
   getRoutes: async () => {
-    var tourpackages = require(dataSource)
+    var tourpackages = require(dataSource);
     tourpackages = tourpackages.sort((a, b) => {
-      return a.total_hours - b.total_hours
-    })
+      return a.total_hours - b.total_hours;
+    });
     return [
       {
-        path: '/',
+        path: "/",
         getData: () => ({
           tourpackages,
         }),
-        children: tourpackages.map(tourpackage => ({
+        children: tourpackages.map((tourpackage) => ({
           path: `/${urlPrefix}/${tourpackage.slug}`,
-          template: 'src/Components/TourPackage',
+          template: "src/Components/TourPackage",
           getData: () => ({
             tourpackage: tourpackage,
-            lastUpdate: format(new Date(), 'MMMM DD, YYYY')
-          })
+            lastUpdate: format(new Date(), "MMMM DD, YYYY"),
+          }),
         })),
       },
       {
-        path: '/philippine-tours-2019-2020/davao-tourpackages',
-        template: 'src/Components/TourPackages',
+        path: "/philippine-tours-2019-2020/davao-tourpackages",
+        template: "src/Components/TourPackages",
         getData: () => ({
           tourpackages,
-        })
+        }),
       },
       {
-        path: '404',
-        template: 'src/Components/404'
-      }
-      
-    ]
+        path: "404",
+        template: "src/Components/404",
+      },
+    ];
   },
   Document: ({ Html, Head, Body, children, siteData, renderMeta }) => {
     let googleTracker = () => {
-      if (process.env.NODE_ENV === 'development') {
-        return `console.log('Dev Mode')`
+      if (process.env.NODE_ENV === "development") {
+        return `console.log('Dev Mode')`;
       } else {
         return `
           (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -53,37 +52,60 @@ export default {
 
           ga('create', 'UA-11499974-1', 'auto');
           ga('send', 'pageview'); 
-        `
+        `;
       }
-    }
+    };
 
     return (
       <Html lang="en-US">
         <Head>
           <meta charSet="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
+          />
           <meta name="theme-color" content="#000000" />
-          <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossOrigin="anonymous" />
-          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossOrigin="anonymous" />
+          <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+            integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+            crossOrigin="anonymous"
+          />
         </Head>
         <Body>
           {children}
           <div id="modal"></div>
-          <script dangerouslySetInnerHTML={{ __html: googleTracker() }}></script>
-          <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossOrigin="anonymous"></script>
-          <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossOrigin="anonymous"></script>
+          <script
+            dangerouslySetInnerHTML={{ __html: googleTracker() }}
+          ></script>
+          <script
+            src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+            crossOrigin="anonymous"
+          ></script>
+          <script
+            src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+            crossOrigin="anonymous"
+          ></script>
         </Body>
       </Html>
-    )
+    );
   },
   plugins: [
     [
-      require.resolve('react-static-plugin-source-filesystem'),
+      require.resolve("react-static-plugin-source-filesystem"),
       {
-        location: path.resolve('./src/pages'),
+        location: path.resolve("./src/pages"),
       },
     ],
-    require.resolve('react-static-plugin-reach-router'),
-    require.resolve('react-static-plugin-sitemap'),
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap"),
   ],
-}
+};
